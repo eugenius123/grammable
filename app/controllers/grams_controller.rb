56a10f41357addca1809f50c7254de5c815fc5f2
eugenius123 <yugene123@gmail.com>
2018-08-12 23:08:24 -0400

    the tests were added for 3 new features

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,21 @@
 class GramsController < ApplicationController
   def index
+    @gram = Gram.new
+  end
+
+  def new
 
   end
+
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    return params.require(:gram).permit(:message)
+  end
+
 end
